da48043d67a86b3367f537b5f10840c30a666b2d
Longxiang <wlongxiang1119@gmail.com>
2018-06-08 11:13:53 +0200

    Refactor script to extract frames from video

--- extract_frames_from_video.py
+++ extract_frames_from_video.py
@@ -1,17 +1,18 @@
 import cv2
 import os
 
-def extract_frames_from_video(video_src, file_name):
+def extract_frames_from_video(video_src, file_name, dir):
     cap = cv2.VideoCapture(video_src)
-    if not os.path.exists("static"):
-        os.mkdir("static")
+    if not os.path.exists(dir):
+        os.mkdir(dir)
     index = 0
     while True:
         res, img_array = cap.read()
         if img_array is None:
             break
-        cv2.imwrite("static/{}-{}".format(file_name, index), img_array)
+        cv2.imwrite("{}/{}-{}".format(dir, index, file_name), img_array)
+        print("extract {}-{} to directory {}".format(index, file_name, dir))
         index = index + 1
 
 if __name__ == '__main__':
-    extract_frames_from_video("videos/traffic18-06-03-20-26-1528050400.mp4", "darknet.png")
+    extract_frames_from_video("sample_videos/traffic_afternoon_18-06-06-15-41-1528292480.mp4", "afternoon.png", "me")
